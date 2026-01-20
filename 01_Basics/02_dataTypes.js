a8288348476d9b1f8350af94f3e85242f44d60bb
Iamparvej07 <parvejansari05786@gmail.com>
2026-01-20 12:21:15 +0000

    container config done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+let age= 24;  // number
+
+let name="Parvej Ansari"//string
+
+let IsStudent=true;  //boolean
+
+let result; // undefined
+
+let marks= null;  //null
+
+let population =12345678902345667890n;  //bignit
+
+let uniqueId = Symbol("Id"); //symb
+
+
+
+console.log(age);
+console.log(name);
+console.log(IsStudent);
+console.log(result);
+console.log(marks);
+console.log(population);
+console.log(uniqueId);
