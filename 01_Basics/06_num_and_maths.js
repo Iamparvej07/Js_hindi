287210cec9f12d7c644c5754e33c418abdd7dbd8
Parvej Ansari <parvejansari05786@gmail.com>
2026-01-21 23:36:13 +0530

    num and math are done

---
+++ 01_Basics/06_num_and_maths.js
@@ -0,0 +1,36 @@
+const score=400
+//console.log(score);
+
+
+const balance= new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const othernumber= 123.4976
+//console.log(othernumber.toPrecision(3));
+
+const hundreds= 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++++++_MATHS_+++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(15,12,33,45,111,345,23,13));
+// console.log(Math.max(5,9,12,3,45,6,66));
+ 
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min +1)) + min);
