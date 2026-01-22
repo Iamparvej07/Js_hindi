9ddcdf3b25781cfba4459eb05ced619e3b2d109f
Parvej Ansari <parvejansari05786@gmail.com>
2026-01-22 19:03:41 +0530

    array config done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+const myArr = [0,2,4,6,7,8]
+const myHeros =["spiderman", "batman","Thor"]
+
+
+const myArr2 = new Array(1,2,3,4,5)
+
+console.log(myArr[1]);
+
+
+// array methods
+
+console.log(myArr);
+console.log(myArr2.includes(9));
+console.log(myArr2.indexOf(5));
+ 
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log( newArr);
+
+
